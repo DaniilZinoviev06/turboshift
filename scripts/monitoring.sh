a8b4686133cbbaf0538f2da3b4711fe0dcaa6a02
DaniilZinoviev06 <daniilzinoviev95@gmail.com>
2024-10-17 18:29:19 +0300

    monitoring

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 monitoringFunc() {
-	SNAP_DIR="/run/timeshift"
-	SNAPSHOT_DIR=$(find "$SNAP_DIR" -type d -regex '.*/[0-9]+$' -print | awk '{print $0 "/backup/timeshift-btrfs/snapshots"}' | head -n 1)
+	PARTITION="/dev/nvme0n1p2"
+	MOUNT_DIR="/run/turboshift"
+
+	sudo mkdir -p "$MOUNT_DIR"
+	sudo mount -o subvol=/ "$PARTITION" "$MOUNT_DIR"
+	  
+	SNAPSHOT_DIR="/run/turboshift/timeshift-btrfs/snapshots"
 
 	WORK_DIR="/tmp/timeshift_compare"
 
 	mkdir -p "$WORK_DIR"
-
 	SNAPSHOTS=("$SNAPSHOT_DIR"/*)
-	echo "$SNAPSHOT_DIR"
-	
+
 	for SNAP in "${SNAPSHOTS[@]}"; do
 		SNAP_NAME=$(basename "$SNAP")
 		echo $SNAP_NAME
 		sudo chroot "$SNAP/@/" pacman -Qq > "$WORK_DIR/packages_${SNAP_NAME}.txt"
 	done
+
+	sudo umount /run/turboshift
 }
