a8c349a28f00070101fd0219cb545f1fe7d93b98
DaniilZinoviev06 <daniilzinoviev95@gmail.com>
2024-10-27 01:03:54 +0300

    small changes

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -2,11 +2,8 @@
 
 ###
 PACKAGES="timeshift cronie grub-btrfs vi"
-USER=$(whoami)
 ###
 #echo "Текущий пользователь: $USER"
-
-echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
 
 install_libraries_arch() {
 	if ! pacman -Q $1 &> /dev/null; then
@@ -22,5 +19,3 @@ for package in $PACKAGES; do
 		echo "Library $package is already installed."
 	fi
 done
-
-sudo rm /etc/sudoers.d/$USER
